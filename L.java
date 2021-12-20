34d8f10de5ceccdb6cbf131977d070ec7346747f
unknown <harshithxworkz@gmail.com>
2021-12-20 21:56:19 +0530

    this is first learning

---
+++ L.java
@@ -0,0 +1,19 @@
+class L{
+               public static void main(String  []args){
+                  long  []harshi  = {10,20,30,40,50,60,70,80,90,100,120,130};
+                  System.out.println(harshi[0]);
+                  System.out.println(harshi[1]);
+                  System.out.println(harshi[2]);
+                  System.out.println(harshi[3]);
+                  System.out.println(harshi[4]);
+                  System.out.println(harshi[5]);
+                   System.out.println(harshi[6]);
+                  System.out.println(harshi[7]);
+                  System.out.println(harshi[8]);
+                  System.out.println(harshi[9]);
+                  System.out.println(harshi[10]);
+                  System.out.println(harshi[11]);
+             
+    }
+}     
+          
